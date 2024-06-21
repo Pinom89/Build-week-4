484e1ce83901aa4ae58194a94115c82981be2dc0
nagcas <studio.nagcas@outlook.it>
2024-06-21 16:47:48 +0200

    add style page notFound

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,15 @@
+import '../style/NotFound.css';
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
-    <div>Non venire qui dentro</div>
+    <Container className='content'>
+      <h1 className='text-dark'>4🤯4</h1>
+      <h4 className='text-dark'>Opps! Page not found</h4>
+      <p className='text-dark'>Siamo spiacenti, la pagina che stai cercando non esiste.</p>
+      <Link to='/' className='btn_back'>Vai a Home</Link>
+    </Container>
   )
 }
